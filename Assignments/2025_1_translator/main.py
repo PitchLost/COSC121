3131a8d599c552868259126b39d2b6db394f9b5b
PitchLost <20067ja@kuranuicollege.school.nz>
2025-03-22 13:28:32 +1300

    Sync

---
+++ Assignments/2025_1_translator/main.py
@@ -0,0 +1,59 @@
+'''Assignment #1'''
+
+
+def get_translations_from_file(translations): 
+    """Extract translations from a string and return a list of tuples."""
+    lines = translations.splitlines()
+    translations_list = [tuple(line.split(',')) for line in lines]
+
+    # Ensure there is at least a header and one translation
+    if len(translations_list) < 2:
+        return "Invalid input format!"
+
+    translations_order = translations_list.pop(0)
+
+    # Validate headers
+    valid_headers = {"māori", "english"}
+    if set(translations_order) !=    valid_headers:
+        print("Header language not recognized!")
+        return []
+
+    # Reverse order if needed
+    if translations_order == ("māori", "english"):
+        translations_list = [(eng, maori) for maori, eng in translations_list]
+
+    return translations_list
+
+
+def reo_test(string):
+    '''Game function'''
+
+    # Now use the get_translations function to convert the string to a list of tuples
+    translations = get_translations(string)
+    len_translations = len(translations)
+
+    # Early exit function if translations were not valid
+    if len_translations < 1:
+        print("We can't play, hōhā!")
+        return -1
+
+    # Game logic
+    print("Kia ora, you have", len_translations,"terms to translate today :-)")
+
+    # Init the score var
+    score = 0
+
+    # Game loop
+    for translation in translations:
+        print('en:', translation[0])
+        users_response =input('mi: ')
+
+        if users_response.lower() == translation[1].lower():
+            print('Ka pai')
+            score += 1
+        else:
+            print('A:',translation[1])
+
+    # Calc score percent
+    final_score = score / len_translations * 100
+    return final_score
